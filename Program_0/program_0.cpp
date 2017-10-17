14b2ac8e1e3f891b474d2075fc26a099c99df663
Michael Balling <mballing@students.mchenry.edu>
2017-10-17 00:29:37 -0500

    Create program_0.cpp

---
+++ Program_0/program_0.cpp
@@ -0,0 +1,21 @@
+/**********************************
+Programmer: Michael Balling 
+ZID: 1758575
+
+Section: 240-2
+
+Date Due: 9/1/2017
+
+Purpose: Compiler training
+***********************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello, world";
+	cout << "\ngoodbye for now...";
+
+	return 0;
+}
